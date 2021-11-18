fbd9a3ae6fe58c433798051af35aa342a1048417
qiqizhouu02 <yz6897@nyu.edu>
2021-11-18 16:48:36 -0500

    added javascript & name

--- javascript-files/midterm.js
+++ javascript-files/midterm.js
@@ -1,16 +1,29 @@
 const app = {
 
     newTextColor: 'blue',
+    oldTextColor: 'white',
 
     initialize: function() {
 
         document.getElementById('headtext').addEventListener('click', () => {
             app.changeTextColor()
         });
-        console.log('help')
+
+        // document.getElementsById('a').addEventListener('hover', () => {
+        //     app.changeTextColor()
+        // });
     },
 
     changeTextColor: function() {
         document.body.style.color = this.newTextColor;
+    },
+
+    changeText2Color: function(event) {
+        event.style.color = this.newTextColor;
+    },
+
+    changeTextBack: function(event) {
+        console.log(event)
+        event.style.color = this.oldTextColor;
     }
 }
