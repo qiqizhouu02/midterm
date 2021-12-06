16807dfd0c5f728063ce2f25d016a3a85246a501
qiqizhouu02 <yz6897@nyu.edu>
2021-12-06 17:02:38 -0500

    attempt at js for main page

---
+++ javascript-files/contact.js
@@ -0,0 +1,13 @@
+// const app = {
+//     initialize: () => {
+//         app.getEntries();
+//     },
+
+//     getEntries: () => {
+//         fetch('https://sheetdb.io/api/v1/yvzor0apt6uax')
+//             .then(result => result.json())
+//             .then(result => {
+//                 $()
+//             })
+//     }
+// }
--- javascript-files/midterm.js
+++ javascript-files/midterm.js
@@ -1,29 +1,44 @@
+// const app = {
+
+//     newTextColor: 'blue',
+//     oldTextColor: 'white',
+
+//     initialize: function() {
+
+//         document.getElementById('headtext').addEventListener('click', () => {
+//             app.changeTextColor()
+//         });
+
+//         // document.getElementsById('a').addEventListener('hover', () => {
+//         //     app.changeTextColor()
+//         // });
+//     },
+
+//     changeTextColor: function() {
+//         document.body.style.color = this.newTextColor;
+//     },
+
+//     changeText2Color: function(event) {
+//         event.style.color = this.newTextColor;
+//     },
+
+//     changeTextBack: function(event) {
+//         console.log(event)
+//         event.style.color = this.oldTextColor;
+//     }
+// }
+
+
 const app = {
+    initialize: function() {}
 
-    newTextColor: 'blue',
-    oldTextColor: 'white',
-
-    initialize: function() {
-
-        document.getElementById('headtext').addEventListener('click', () => {
-            app.changeTextColor()
-        });
-
-        // document.getElementsById('a').addEventListener('hover', () => {
-        //     app.changeTextColor()
-        // });
-    },
-
-    changeTextColor: function() {
-        document.body.style.color = this.newTextColor;
-    },
-
-    changeText2Color: function(event) {
-        event.style.color = this.newTextColor;
-    },
-
-    changeTextBack: function(event) {
-        console.log(event)
-        event.style.color = this.oldTextColor;
-    }
 }
+
+$(document).ready(function() {
+    $("figure").mouseover(function() {
+        $(this).addClass("overlay", "text");
+    });
+    $("figure").mouseleave(function() {
+        $(this).removeClass("overlay", "text");
+    });
+});
